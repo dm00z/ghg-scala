d545e98e3af3a20c20cbc6b972628b5ae039d41c
giabao <giabao@sandinh.net>
2015-12-08 14:59:16 +0700

    remove LeftNavPage

--- src/main/scala/ghg/AppCSS.scala
+++ src/main/scala/ghg/AppCSS.scala
@@ -1,28 +1,20 @@
 package ghg
 
 import chandu0101.scalajs.react.components._
-import ghg.components.{AppHeader, LeftNavPage, LeftNav}
+import ghg.components.LeftNav
+import ghg.pages.GhgPage
 import scalacss.mutable.GlobalRegistry
 import scalacss.Defaults._
 import scalacss.ScalaCssReact._
 
 object AppCSS {
   def load() = {
-    GlobalRegistry.register(LeftNav.Style,
-      LeftNavPage.Style,
-//      MuiPaperDemo.Style,
-//      MuiSwitchesDemo.Style,
-//      MuiMenuDemo.Style,
-//      MobileTearSheet.Style,
+    GlobalRegistry.register(
+      LeftNav.Style,
+      GhgPage.Style,
       ReactTable.DefaultStyle,
       ReactListView.DefaultStyle,
-//      ReactSearchBox.DefaultStyle,
       Pager.DefaultStyle
-//      ScalaCSSTutorial.Style,
-//      InfoTemplate.Style,
-//      ReactInfiniteDemo.styles,
-//      ReactDraggable.Style,
-//      MuiTabsDemo.Style
     )
     GlobalRegistry.addToDocumentOnRegistration()
   }
--- src/main/scala/ghg/pages/GhgPage.scala
+++ src/main/scala/ghg/pages/GhgPage.scala
@@ -1,18 +1,37 @@
 package ghg.pages
 
 import diode.react.ModelProxy
-import ghg.components.LeftNavPage
+import ghg.components.LeftNav
 import ghg.routes.{AppRoutes, AppRoute}
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.extra.router.RouterCtl
+import japgolly.scalajs.react.vdom.prefix_<^._
 import model.GhgData
+import scalacss.Defaults._
+import scalacss.ScalaCssReact._
 
 object GhgPage {
-  case class Backend($: BackendScope[Props, _]) {
-    def render(P: Props) = LeftNavPage(AppRoutes.all, P.selectedPage, P.ctrl)
+  object Style extends StyleSheet.Inline { import dsl._
+    val container = style(
+      display.flex,
+      minHeight(600.px))
+    val nav = style(width(190.px))
+    val content = style(
+      padding(10.px),
+      borderLeft :=! "1px solid rgb(223, 220, 220)",
+      flex := "1")
   }
 
-  val component = ReactComponentB[Props]("MuiPage")
+  case class Backend($: BackendScope[Props, _]) {
+    def render(P: Props) = {
+      <.div(Style.container,
+        <.div(Style.nav, LeftNav(AppRoutes.all, P.selectedPage, P.ctrl)),
+        <.div(Style.content, P.selectedPage.render())
+      )
+    }
+  }
+
+  val component = ReactComponentB[Props]("GhgPage")
     .renderBackend[Backend]
 //    .configureSpec(ThemeInstaller.installMuiContext())
     .build
