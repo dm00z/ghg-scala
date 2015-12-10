97dee3a5e09e4f1059f08a6409aec46743623fc2
giabao <giabao@sandinh.net>
2015-12-10 16:28:26 +0700

    skeleton for GasPage

--- src/main/scala/ghg/pages/GasPage.scala
+++ src/main/scala/ghg/pages/GasPage.scala
@@ -1,15 +1,27 @@
 package ghg.pages
 
 import diode.react.ModelProxy
-import japgolly.scalajs.react.ReactComponentB
+import japgolly.scalajs.react.{BackendScope, ReactComponentB}
 import japgolly.scalajs.react.vdom.prefix_<^._
 import model.GhgData
+import scala.language.existentials
 
 object GasPage {
-  val component = ReactComponentB[Unit]("Gas")
-    .render(_ =>
-      <.div("(gas page)")
-    ).buildU
+  type Props = ModelProxy[GhgData]
+  implicit final class PropsEx(val p: Props) extends AnyVal {
+    @inline def my = p.zoom(_.indirect.gas)
+  }
 
-  def apply(d: ModelProxy[GhgData]) = component()
+  case class Backend($: BackendScope[Props, _]) {
+    def render(P: Props) = {
+      val my = P.my()
+      <.div("(gas page)")
+    }
+  }
+
+  val component = ReactComponentB[Props]("Gas")
+    .renderBackend[Backend]
+    .build
+
+  def apply(d: ModelProxy[GhgData]) = component(d)
 }
