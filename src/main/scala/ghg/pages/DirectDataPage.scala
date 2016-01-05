8095ec3a6e6d30a04a07379267cbc9c596ac3789
giabao <giabao@sandinh.net>
2016-01-05 13:56:57 +0700

    implementing DirectDataPage

--- src/main/scala/ghg/pages/DirectDataPage.scala
+++ src/main/scala/ghg/pages/DirectDataPage.scala
@@ -1,15 +1,62 @@
 package ghg.pages
 
 import diode.react.ModelProxy
-import japgolly.scalajs.react.ReactComponentB
+import ghg.Utils._
+import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
 import model.GhgData
+import model.DirectTable._
 
 object DirectDataPage {
-  val component = ReactComponentB[Unit]("DirectData")
-    .render(_ =>
-      <.div("(DirectData page)")
-    ).buildU
+  type Props = ModelProxy[GhgData]
+  implicit final class PropsEx(val p: Props) extends AnyVal {
+    @inline def my = p.zoom(_.direct.d)
+  }
 
-  def apply(d: ModelProxy[GhgData]) = component()
+  case class Backend($: BackendScope[Props, _]) {
+    def render(P: Props) = {
+      val my = P.my
+
+      def genericTbl(implicit d: GenericData) = {
+        @inline implicit def dispatch: GenericData => Callback = my.dispatch
+
+        table(
+          <.tr(
+            <.td(<.b("Thông số")),
+            <.td(<.b("Đơn vị")),
+            <.td(<.b("Giá trị")),
+            <.td(<.b("Ghi chú"))
+          ),
+          <.tr(td("Q", "o,v"), <.td("m3/day"), <.td(P().info.power),
+            <.td("Lưu lượng dòng vào hệ thống xử lý")
+          ),
+          <.tr(td("S", "o,v"), <.td("mg/l"),
+            tdInput(GenericData.s),
+            <.td("Nồng độ cơ chất dòng vào hệ thống")
+          ),
+          <.tr(td("TKN", "v"), <.td("mg/l"),
+            tdInput(GenericData.tkn),
+            <.td()
+          ),
+          <.tr(<.td("TSS"), <.td("mg/l"),
+            tdInput(GenericData.tss),
+            <.td()
+          )
+        )
+      }
+
+      val d = my()
+      <.div(
+        <.h3("1. Thông số dòng vào hệ thống xử lý"),
+        genericTbl(d.generic)
+      )
+
+    }
+  }
+
+  val component = ReactComponentB[Props]("DirectData")
+    .renderBackend[Backend]
+    .build
+
+  def apply(d: ModelProxy[GhgData]) = component(d)
 }
