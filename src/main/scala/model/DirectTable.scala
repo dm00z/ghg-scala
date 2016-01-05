59255057caf581a1a51543a4b8f8792422a92a77
giabao <giabao@sandinh.net>
2016-01-05 13:56:38 +0700

    misc

--- src/main/scala/model/DirectTable.scala
+++ src/main/scala/model/DirectTable.scala
@@ -1,16 +1,20 @@
 package model
 
+import monocle.macros.Lenses
+
 object DirectTable {
 
   /** Thông số dòng vào hệ thống xử lý
+    *
     * @note q: Double = 8000 (m3/day) is taken from InfoData
     * @param s mg/l
     * @param tkn mg/l
     * @param tss mg/l */
-  case class GenericData(s: Double, tkn: Double, tss: Double)
+  @Lenses case class GenericData(s: Double, tkn: Double, tss: Double)
 
   /** Thông số bể xử lý sơ cấp
     * Tài liệu tham khảo 	Metcalf and Eddy (2002)
+    *
     * @param prBOD (%) value in rang [0,1]
     * @param prSS (%) value in rang [0,1]
     * @param q m3/day
@@ -18,17 +22,18 @@ object DirectTable {
   case class PrimaryPoolData(prBOD: Double, prSS: Double, q: Double)
 
   /** Thông số bể yếm khí hoặc hiếu khí
+    *
     * @param t oC
     * @param srt day
     * @param hrtOpt hour
     * @param vOpt m3
-    *
     * @note sv, s, x, q are derivative
     *       v and hrt have a relation (v = hrt / q), we must provide only one value.
     */
   case class PoolData(t: Int, srt: Double, hrtOpt: Option[Double], vOpt: Option[Double] = None)
 
   /** Thông số dòng ra hệ thống xử lý
+    *
     * @param t oC
     * @param n mg/l
     * @param vss mg/l
--- src/main/scala/model/InfoData.scala
+++ src/main/scala/model/InfoData.scala
@@ -2,6 +2,6 @@ package model
 
 case class Plant(tpe: String, name: String, addr: String)
 
-/** @param power TODO đơn vị?
+/** @param power m3/day - Lưu lượng dòng vào hệ thống xử lý
   * @note Công nghệ xử lý hiện không dùng trong tính toán */
 case class InfoData(f: Plant, power: Double)
