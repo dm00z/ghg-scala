fbf934bd17af747c516700727544d6c4b72f9f8d
giabao <giabao@sandinh.net>
2016-01-12 21:28:31 +0700

    enable print

--- main.js
+++ main.js
@@ -1,6 +1,69 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
+var Menu = require('menu');
+var template = [
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Print',
+                accelerator: 'CmdOrCtrl+P',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.print();
+                }
+            },
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.reload();
+                }
+            },
+            {
+                label: 'Toggle Full Screen',
+                accelerator: (function() {
+                    if (process.platform == 'darwin')
+                        return 'Ctrl+Command+F';
+                    else
+                        return 'F11';
+                })(),
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                }
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: (function() {
+                    if (process.platform == 'darwin')
+                        return 'Alt+Command+I';
+                    else
+                        return 'Ctrl+Shift+I';
+                })(),
+                click: function(item, focusedWindow) {
+                    if (focusedWindow)
+                        focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Help',
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click: function() { require('electron').shell.openExternal('http://electron.atom.io') }
+            }
+        ]
+    }
+];
+var menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
 // Report crashes to our server.
 require('crash-reporter').start();
 
