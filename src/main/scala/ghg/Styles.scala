5385c800b6717ac5008847e0e21018c3336095d8
dm00z <tt01999@gmail.com>
2018-09-02 12:54:35 +0700

    Update dependencies, GUI

--- src/main/scala/ghg/Styles.scala
+++ src/main/scala/ghg/Styles.scala
@@ -17,4 +17,54 @@ object Styles extends StyleSheet.Standalone {
     stroke(steelblue),
     strokeWidth(1.5.px)
   )
+
+  "body" - (
+    margin.`0`,
+    position.relative,
+    height :=! "100%"
+  )
+
+  "table" - (
+    marginTop(10.px),
+    borderCollapse.collapse,
+    borderSpacing(2.px),
+    width :=! "85%",
+    backgroundColor.white,
+    borderRadius(5.px),
+    overflow.hidden,
+    boxShadow := ("0 0px 40px 0px rgba(0, 0, 0, 0.15)")
+  )
+
+  "tr" - (
+    display.tableRow,
+    verticalAlign.inherit,
+    borderColor.inherit
+  )
+
+  "th, td" - (
+    //fontWeight.unset,
+    paddingRight(10.px),
+    borderBottom(2.px, solid),
+    paddingTop(15.px),
+    paddingBottom(15.px),
+    paddingLeft(20.px)
+  )
+
+  "input" - (
+    fontSize(14.px),
+    //padding(5.px),
+    borderTopColor.transparent,
+    borderLeftColor.transparent,
+    borderRightColor.transparent,
+    borderBottomColor :=! ("#21A8DE"),
+    borderBottomWidth(1.px),
+    backgroundColor.transparent
+  )
+
+  "hr" - (
+    margin.`0`,
+    borderStyle.solid,
+    borderTopWidth(0.5.px),
+    borderTopColor :=! "#0275d8"
+  )
 }
